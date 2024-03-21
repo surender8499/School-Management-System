155c0ee68a089569f0c622d13696b11e5a5eba19
surender8499 <37922321+surender8499@users.noreply.github.com>
2024-03-21 12:14:27 +0530

    Backend code

---
+++ backend/students/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    studentId = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    RegistrationNo = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+    Course = models.CharField(max_length=100)
---
+++ backend/students/views.py
@@ -0,0 +1,48 @@
+from django.http.response import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http.response import JsonResponse
+from .models import Student
+from .serializers import StudentSerializer
+
+
+class StudentView(APIView):
+
+    def get_student(self, pk):
+        try:
+            student = Student.objects.get(studentId=pk)
+            return student
+        except Student.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk=None):
+        if pk:
+            data = self.get_student(pk)
+            serializer = StudentSerializer(data)
+        else:
+            data = Student.objects.all()
+            serializer = StudentSerializer(data, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = request.data
+        serializer = StudentSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Student Added Successfully", safe=False)
+        return JsonResponse("Failed to Add Student", safe=False)
+
+    def put(self, request, pk=None):
+        student_to_update = Student.objects.get(studentId=pk)
+        serializer = StudentSerializer(instance=student_to_update, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Student updated Successfully", safe=False)
+        return JsonResponse("Failed To Update Student")
+
+    def delete(self, request, pk):
+        student_to_delete = Student.objects.get(studentId=pk)
+        student_to_delete.delete()
+        return JsonResponse("Student Deleted Successfully", safe=False)
